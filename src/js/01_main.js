7df76d3f7cf3658651f145f75e4e1a3306744e0a
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-09-01 16:59:23 +0300

    finall develop

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -83,3 +83,52 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
+    anchor.addEventListener('click', function (e) { 
+        e.preventDefault(); 
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ 
+            behavior: 'smooth' 
+        }); 
+    }); 
+});
+
+
+function updateCopyrightYear(startYear) {
+    let currentYear = new Date().getFullYear();
+    let yearSpan = document.getElementById('year');
+
+    if (currentYear > startYear) {
+        yearSpan.textContent = `${startYear} - ${currentYear}`;
+    } else {
+        yearSpan.textContent = startYear;
+    }
+}
+
+// Вызов функции с годом создания сайта
+updateCopyrightYear(2024);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const certImage = document.getElementById('certImage');
+    const popup = document.getElementById('popup');
+    const popupImage = document.getElementById('popupImage');
+    const closePopup = document.getElementById('closePopup');
+
+    certImage.addEventListener('click', function() {
+        popup.style.display = 'block';
+        popupImage.src = certImage.src; // Устанавливаем источник изображения в попапе
+    });
+
+    closePopup.addEventListener('click', function() {
+        popup.style.display = 'none'; // Закрываем попап
+    });
+
+    // Закрытие попапа при клике вне изображения
+    popup.addEventListener('click', function(event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
